c4377f00000c95b1dc12b1886610f1a908fef53a
Rin <ashja234@gmail.com>
2022-06-30 16:10:00 +0700

    .env creation

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = `https://hknnhacamqrcalaqobvh.supabase.co`
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNjEzMDk0MCwiZXhwIjoxOTMxNzA2OTQwfQ.k22HKqpD5_rYVijuMXjCt2sskq1x0fIQNvFp_FFp6Ws"
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
